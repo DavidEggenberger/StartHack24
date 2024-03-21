6a6b879408f2f4ff71a7c3c9a8106db0ec0b57df
David Eggenberger <David.Eggenberger@advellence.com>
2024-03-22 00:24:44 +0100

    twilio webhook

--- Source/Server/Features/WhatsAppMessaging/TwilioWebHook.cs
+++ Source/Server/Features/WhatsAppMessaging/TwilioWebHook.cs
@@ -1,5 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+using System;
+using Twilio.TwiML.Messaging;
+using Twilio.TwiML;
+using Twilio.AspNet.Core;
+using System.Threading.Tasks;
 
 namespace Server.Features.WhatsAppMessaging
 {
@@ -7,6 +16,21 @@ namespace Server.Features.WhatsAppMessaging
     [ApiController]
     public class TwilioWebHook : ControllerBase
     {
+        [HttpPost]
+        public async Task<ActionResult> Create()
+        {
+            var requestBody = Request.Form["Body"];
+            var response = new MessagingResponse();
+            if (requestBody == "hello")
+            {
+                response.Message("Hi!");
+            }
+            else if (requestBody == "bye")
+            {
+                response.Message("Goodbye");
+            }
 
+            return Ok();
+        }
     }
 }
